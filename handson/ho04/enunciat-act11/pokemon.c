2fdd564e198d0bbdf604468593f1e6b238880b6a
David Argente <daviidsolv@gmail.com>
2022-10-24 16:34:22 +0200

    completed pokemon.c

--- handson/ho04/enunciat-act11/pokemon.c
+++ handson/ho04/enunciat-act11/pokemon.c
@@ -23,6 +23,7 @@ void init_pokedex(void){
     perror("fopen");
     exit(1);
   }
+<<<<<<< HEAD
 
   char *buf = malloc(POKEMONS);
   char *info;
@@ -52,5 +53,56 @@ void init_pokedex(void){
 
   fclose(f);
   free(buf);
+=======
+  //@TODO
+
+  char *buffer = malloc(151);
+
+  while(fgets(buffer, 151, f) != NULL) {
+    char *token = strtok(buffer, ",");
+    int id = atoi(token);
+
+    token = strtok(NULL, ",");
+    char *name = token;
+
+    token = strtok(NULL, ",");
+    char *type = token;
+
+    token = strtok(NULL, ",");
+    char *type2 = token;
+
+    token = strtok(NULL, ",");
+    int total = atoi(token);
+    
+    token = strtok(NULL, ",");
+    int hp = atoi(token);
+
+    token = strtok(NULL, ",");
+    int attack = atoi(token);
+
+    token = strtok(NULL, ",");
+    int deffense = atoi(token);
+
+    token = strtok(NULL, ",");
+    int spAttack = atoi(token);
+
+    token = strtok(NULL, ",");
+    int spDeffense = atoi(token);
+
+    token = strtok(NULL, ",");
+    int speed = atoi(token);
+
+    token = strtok(NULL, ",");
+    int generation = atoi(token);
+
+    token = strtok(NULL, ",");
+    int legendary = atoi(token);
+
+    struct pokemon p = {id, name, type, type2, total, hp, attack, defense, spAttack, spDeffense, speed, generation, legendary};
+    pokedex[id] = pokemon;
+  }
+
+  fclose(f);
+>>>>>>> 48042f7 (completed pokemon.c)
 }
 
