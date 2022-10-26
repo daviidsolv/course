96504913b624b13afc2cacb98055d70a7130a78d
David Argente <daviidsolv@gmail.com>
2022-10-26 18:19:55 +0200

    main_done

--- handson/ho04/enunciat-act11/main.c
+++ handson/ho04/enunciat-act11/main.c
@@ -7,4 +7,28 @@
 int
 main(int argc, char *argv[])
 {
+
+    int status;
+    pid_t pid;
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    // Executa fill
+    if (pid == 0) {
+        execl("./random", "random", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    // Executa pare
+    init_pokedex();
+    if (waitpid(pid, &status, 0) >= 0) {
+        if (WIFEXITED(status))
+            show_pokemon(WEXITSTATUS(status));
+    }
+
 }
