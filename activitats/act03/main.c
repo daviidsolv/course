53574ab1a5ebf4668d5a3be9b9ea25119e210b4b
David Argente <daviidsolv@gmail.com>
2022-11-05 17:00:46 +0100

    simple commands parsing

--- activitats/act03/main.c
+++ activitats/act03/main.c
@@ -1,8 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "parsing.h"
 
-int
-main(int argc, char *argv[]){
-    // while not comand exit
-        // readline
-        // parseline
-        // launch comand
+int main(int argc, char *argv[]){
+    char *command = malloc(100);
+
+    while(memcmp(command, "exit", 4) != 0) {
+        printf("%sossh%s> %s", KGRN ,KRED, KNRM);
+
+        fgets(command, 100, stdin);
+        char *cmd = malloc(strlen(command)-1);
+        strncpy(cmd, command, strlen(command) - 1);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            execlp(cmd, cmd, (char *)NULL);
+            exit(0);
+        }
+        else {
+            waitpid(pid, NULL, 0);
+        }
+    }
+
+    return 0;
 }
